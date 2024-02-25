de29422d7aa1662e342e1146c5214fb625f838e1
tanujzode <tanuj.zode@gmail.com>
2024-02-26 02:34:23 +0530

    initial project uploaded

---
+++ orm_and_frontend/product/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from product.models import ProductTable, CartTable, CustomerDetails
+
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'details', 'category', 'is_active', 'rating', 'image']
+
+admin.site.register(ProductTable, ProductAdmin)
+admin.site.register(CartTable)
+admin.site.register(CustomerDetails)
